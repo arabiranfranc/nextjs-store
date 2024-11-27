1629575dcb67ef1e6d9c40515eb379ea503b5b16
sic <arabiran@prans.com>
2024-11-28 07:13:31 +0800

    use product id

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -2,7 +2,7 @@ import { Button } from "../ui/button";
 
 const AddToCart = ({ productId }: { productId: string }) => {
   return (
-    <Button className="capitalize mt-8" size="lg">
+    <Button className="capitalize mt-8" size="lg" id={productId}>
       add to cart
     </Button>
   );
--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -7,7 +7,7 @@ const ProductRating = async ({ productId }: { productId: string }) => {
   const className = `flex gap-1 items-center text-md mt-1 mb-4`;
   const countValue = `(${count}) reviews`;
   return (
-    <span className={className}>
+    <span className={className} id={productId}>
       <FaStar className="w-3 h-3" />
       {rating} {countValue}
     </span>
